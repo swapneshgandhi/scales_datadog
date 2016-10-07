854c72bc3c4e32d0d5b0e937da1acaa7c6e598bc
sgandhi <sgandhi@ooyala.com>
2016-10-07 11:32:26 -0700

    bump version

--- scales_datadog/__init__.py
+++ scales_datadog/__init__.py
@@ -4,7 +4,7 @@ __credits__ = []
 
 
 __license__ = 'BSD'
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 __maintainer__ = 'Tommaso Barbugli'
 __email__ = 'tbarbugli@gmail.com'
 __status__ = 'Production'
